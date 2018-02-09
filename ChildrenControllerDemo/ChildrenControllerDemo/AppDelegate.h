0eb0c5dc0017f4b765f73c664101a3b72c10ccb3
wuruiqing <929135662@qq.com>
2018-02-10 01:18:51 +0800

    create project

---
+++ ChildrenControllerDemo/ChildrenControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChildrenControllerDemo
+//
+//  Created by WRQ on 2018/2/10.
+//  Copyright © 2018年 WRQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChildrenControllerDemo/ChildrenControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChildrenControllerDemo
+//
+//  Created by WRQ on 2018/2/10.
+//  Copyright © 2018年 WRQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
